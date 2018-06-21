659102fac20fa61fd753cb9a25bf412d30f1d879
Ibraheem Khan <m_i_khan@outlook.com>
2018-06-21 17:43:23 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { Pro } from '@ionic/pro';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -9,6 +10,10 @@ import { HomePage } from '../pages/home/home';
 import { BookPage } from '../pages/book/book';
 import { TocPage } from '../pages/toc/toc';
 import { SettingsPage } from '../pages/settings/settings';
+
+Pro.init('f9f2f3cc', {
+  appVersion: '1.0'
+})
 
 @NgModule({
   declarations: [
